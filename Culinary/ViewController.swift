79529b6029368def3b9ec9fc08465e1f0648f107
Sokratis <metalsok@gmail.com>
2020-04-07 11:20:48 +0200

    Initial Commit

---
+++ Culinary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Culinary
+//
+//  Created by Sokratis Metallidis on 07.04.20.
+//  Copyright © 2020 The Bobos Family. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
